b5f9194b6013567b317e554a2d973db0bce46313
RunasSudo <runassudo@yingtongli.me>
2017-12-10 23:19:07 +1030

    Add COPYING

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+#   pyWSDscan
+#   Copyright © 2017  RunasSudo (Yingtong Li)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # https://msdn.microsoft.com/en-us/library/windows/hardware/dn641604(v=vs.85).aspx
 # https://docs.microsoft.com/en-us/windows-hardware/drivers/image/web-services-on-devices-reference
